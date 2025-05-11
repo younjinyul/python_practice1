e192b33119b54e23c8a1853d919033798a813c66
Jinyeul <yjy1047@gmail.com>
2025-05-11 10:37:56 +0900

    자료 구조 익히기 숙제

---
+++ homework_for_learn_dictionary.py
@@ -0,0 +1,9 @@
+"""
+숙제
+1. a 라는 이름의 딕셔너리 생성
+2. a 딕셔너리에 "jinyeol" 이라는 key에 1958 이라는 value 추가
+3. a 딕셔너리 출력
+4. a 딕셔너리에 원하는 key와 value 추가
+5. a 에서 jinyeol 이라는 key의 value 출력하기
+6. a에서 jinyeol 이라는 key 삭제
+"""
